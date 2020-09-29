75379b6b52304558f407a0fe47c399f277edbf0d
Siydtti <1570297098@qq.com>
2020-09-29 09:44:31 +0800

    新增页面上传

---
+++ pagesA/groupPurchase/groupPurchase.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="groupPurchase">
+		
+		<view class="store_banner">
+			<swiper class="swiper" autoplay="ture" :interval="interval" :duration="duration" style="height: 100%;">
+				<swiper-item>
+					<view class="swiper-item uni-bg-red">
+						<image src="../../static/images/index_banner.png" style="width: 700rpx;height: 100%;border-radius: 20rpx;"></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-green">
+						<image src="../../static/images/index_banner.png" style="width: 700rpx;height: 400rpx;border-radius: 20rpx;"></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-blue">
+						<image src="../../static/images/index_banner.png" style="width: 700rpx;height: 400rpx;border-radius: 20rpx;"></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-red">
+						<image src="../../static/images/index_banner.png" style="width: 700rpx;height: 400rpx;border-radius: 20rpx;"></image>
+					</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-red">
+						<image src="../../static/images/index_banner.png" style="width: 700rpx;height: 400rpx;border-radius: 20rpx;"></image>
+					</view>
+				</swiper-item>
+			</swiper>
+		
+			<view class="store_bannerBtn_box">
+				<view class="store_bannerBtn_video">
+					<image src="../../static/images/ICON/store_video.png" style="width: 30rpx;height: 30rpx;margin-right: 10rpx;"></image>
+					<text>13</text>
+				</view>
+				<view class="store_bannerBtn_image">
+					<image src="../../static/images/ICON/store_image.png" style="width: 30rpx;height: 30rpx;margin-right: 10rpx;"></image>
+					<text>13</text>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				interval: 2000, //自动切换时间间隔
+				duration: 500, //滑动动画时长
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+@import url("./style/groupPurchase.css");
+</style>
---
+++ pagesA/payment/payment.vue
@@ -0,0 +1,102 @@
+<template>
+	<view class="payment">
+		<view class="payment_courseName">
+			<image src="/static/images/payment_course.png" style="width: 220rpx;height: 160rpx;border-radius: 10rpx;margin: 20rpx 20rpx 20rpx 30rpx ;"></image>
+			<view class="courseName">
+				少儿流利英语口语
+			</view>
+		</view>
+
+		<view class="payment_schollName">
+			<text>学校名称</text>
+			<text>新东方培训学校</text>
+		</view>
+
+		<view class="payment_number payment_list">
+			<view class="left">
+				数量
+			</view>
+			<view class="right">
+				<van-stepper value="1" integer button-size="50rpx" input-width="70rpx" />
+			</view>
+		</view>
+
+		<view class="payment_unitPrice payment_list">
+			<view class="left">
+				小计
+			</view>
+			<view class="right">
+				￥666.6
+			</view>
+		</view>
+
+		<view class="payment_allPrice payment_list">
+			<view class="left">
+				总价
+			</view>
+			<view class="right">
+				<text>￥366.6</text>
+				<text>（已优惠:￥300)</text>
+			</view>
+		</view>
+
+		<view class="payment_groupNumber payment_list">
+			<view class="left">
+				选择拼图人数
+			</view>
+			<view class="right">
+				<van-stepper value="1" integer button-size="50rpx" input-width="70rpx" />
+			</view>
+		</view>
+
+		<view class="payment_hintText">
+			课程购买后，商户可能会和您联系，约定到店时间
+		</view>
+
+		<view class="payment_phone">
+			<view class="left">
+				手机号码
+			</view>
+			<view class="right">
+				156****2089
+			</view>
+		</view>
+
+		<van-checkbox class="payment_check" :value="check" checked-color="#0BBF63" @change="checkChange" icon-size="25rpx">
+			7天无理由
+		</van-checkbox>
+
+		<view class="payment_payBtn">
+			总计：666.6元 去支付
+		</view>
+
+		<view class="payment_inviteBtn" @click="go_groupPurchase">
+			不支付，邀请好友
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				check: true, //7天无理由
+			}
+		},
+		methods: {
+			checkChange() {
+				this.check = !this.check
+			},
+			go_groupPurchase() {
+				uni.navigateTo({
+					url:'../groupPurchase/groupPurchase'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("./style/payment.css");
+</style>
